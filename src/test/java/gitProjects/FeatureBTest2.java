794af93ccece78f10994db374370ca4a357c520d
ebbay <ebbaydev@gmail.com>
2021-09-11 21:43:26 +0200

    My commit to featureB branch

--- src/test/java/gitProjects/FeatureBTest2.java
+++ src/test/java/gitProjects/FeatureBTest2.java
@@ -3,5 +3,7 @@ package gitProjects;
 public class FeatureBTest2 {
     public static void main(String[] args) {
         System.out.println("I added new work in feature B");
+
+        System.out.println("I added more features");
     }
 }
